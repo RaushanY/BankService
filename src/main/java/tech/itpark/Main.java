c99f111dea8cea8d992ef23ef742f5cb25f328ca
RaushanY <raushan4066@gmail.com>
2021-03-23 20:42:50 +0300

    Main.java added

---
+++ src/main/java/tech/itpark/Main.java
@@ -0,0 +1,13 @@
+package tech.itpark;
+
+public class Main {
+    public static void main(String[] args) {
+        double rate = 3;
+        double sum = 1000;
+        double percent = 100;
+        double investment = 1000;
+        double result = rate * sum / percent + investment;
+        System.out.println(result);
+    }
+}
+
